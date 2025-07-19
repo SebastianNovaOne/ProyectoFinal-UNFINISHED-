4c697ef260cd9b47e5670f410ec609c4234686c1
SebastianNovaOne <sebaxnovax@gmail.com>
2025-07-19 07:55:22 -0400

    Se añade documentacion Javadoc.

--- src/main/java/logica/EncuestaNivel.java
+++ src/main/java/logica/EncuestaNivel.java
@@ -2,13 +2,31 @@ package logica;
 
 import java.util.List;
 
+/**
+ * La clase {@code EncuestaNivel} representa una encuesta compuesta por un set de preguntas.
+ * Proporciona un mecanismo para obtener las preguntas que componen la encuesta.
+ */
 public class EncuestaNivel {
+
+    /**
+     * Lista de preguntas que forman la encuesta.
+     */
     private List<Pregunta> preguntas;
 
+    /**
+     * Constructor que inicia una nueva encuesta con una lista de preguntas.
+     *
+     * @param preguntas La lista de preguntas que forman la encuesta.
+     */
     public EncuestaNivel(List<Pregunta> preguntas) {
         this.preguntas = preguntas;
     }
 
+    /**
+     * Obtiene la lista de preguntas que conforman la encuesta.
+     *
+     * @return Una lista de objetos {@link Pregunta}.
+     */
     public List<Pregunta> obtenerPreguntas() {
         return preguntas;
     }

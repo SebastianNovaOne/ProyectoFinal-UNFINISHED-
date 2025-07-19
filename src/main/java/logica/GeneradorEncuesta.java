26c5eacaf3028396f95f42e8287a906378037c83
SebastianNovaOne <sebaxnovax@gmail.com>
2025-07-19 08:04:18 -0400

    Se crea clase GeneradorEncuesta. se añade metodo estatico que despues de completarlo generara la encuesta dependiendo de la eleccion del usuario.

---
+++ src/main/java/logica/GeneradorEncuesta.java
@@ -0,0 +1,13 @@
+package logica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneradorEncuesta {
+
+    public static List<Pregunta> obtenerPreguntasPara(Prueba prueba) {
+        List<Pregunta> preguntas = new ArrayList<>();
+
+        return preguntas;
+    }
+}

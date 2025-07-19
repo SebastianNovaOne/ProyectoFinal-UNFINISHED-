3ebb88b1df0788be4f291ae38bbfae13a1c5d187
SebastianNovaOne <sebaxnovax@gmail.com>
2025-07-19 07:28:28 -0400

    Se añade metodo mostrar a la clase Pregunta.

--- src/main/java/logica/Pregunta.java
+++ src/main/java/logica/Pregunta.java
@@ -18,4 +18,11 @@ public class Pregunta {
     public List<String> getOpciones() {
         return opciones;
     }
+
+    public void mostrar() {
+        System.out.println(enunciado);
+        for (int i = 0; i < opciones.size(); i++) {
+            System.out.println((i + 1) + ". " + opciones.get(i));
+        }
+    }
 }

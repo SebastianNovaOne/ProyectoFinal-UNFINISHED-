cd3faa434259a58540c882279435605d13407de9
SebastianNovaOne <sebaxnovax@gmail.com>
2025-07-19 07:47:24 -0400

    Se añade constructor de la clase EncuestaNivel. Se añade metodo obtenerPreguntas().

--- src/main/java/logica/EncuestaNivel.java
+++ src/main/java/logica/EncuestaNivel.java
@@ -4,4 +4,12 @@ import java.util.List;
 
 public class EncuestaNivel {
     private List<Pregunta> preguntas;
+
+    public EncuestaNivel(List<Pregunta> preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    public List<Pregunta> obtenerPreguntas() {
+        return preguntas;
+    }
 }
